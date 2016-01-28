4cfd58b3d5af62691903de5b7c6a516ae350d550
Guy Cole <guycole@gmail.com>
2016-01-28 01:53:45 +0000

    production version

--- mysql/choral-touch.sql
+++ mysql/choral-touch.sql
@@ -15,7 +15,7 @@ CREATE  TABLE IF NOT EXISTS `choral_touch`.`event` (
   `month` INT NOT NULL ,
   `day` INT NOT NULL ,
   `year` INT NOT NULL ,
-  `note` VARCHAR(128) NULL ,
+  `note` VARCHAR(128) NOT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
 ENGINE = InnoDB;
