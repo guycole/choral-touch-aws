731682ad522517338b20ad041e236d63469406e1
Guy Cole <guycole@gmail.com>
2018-02-01 17:55:26 +0000

    update schema for date

--- mysql/test.sql
+++ mysql/test.sql
@@ -1,8 +1,8 @@
-insert into event(month, day, year, note) values(1, 1, 2001, 'New Year');
-insert into event(month, day, year, note) values(1, 1, 0, 'New Year');
-insert into event(month, day, year, note) values(1, 3, 2009, '3 january');
-insert into event(month, day, year, note) values(12, 30, 2005, 'almost end of year');
-insert into event(month, day, year, note) values(12, 31, 0, 'end of year');
-insert into event(month, day, year, note) values(1, 10, 0, 'jan tenth');
-insert into event(month, day, year, note) values(1, 11, 0, 'jan eleventh');
-insert into event(month, day, year, note) values(1, 12, 0, 'jan twelve');
+insert into event(month, day, year, date, note) values(1, 1, 2001, '2001-1-1', 'New Year');
+insert into event(month, day, year, date, note) values(1, 1, 1980, '1980-1-1', 'New Year');
+insert into event(month, day, year, date, note) values(1, 3, 2009, '2009-1-3', '3 january');
+insert into event(month, day, year, date, note) values(12, 30, 2005, '2005-12-30', 'almost end of year');
+insert into event(month, day, year, date, note) values(12, 31, 1980, '1980-12-31', 'end of year');
+insert into event(month, day, year, date, note) values(1, 10, 1980, '1980-1-10', 'jan tenth');
+insert into event(month, day, year, date, note) values(1, 11, 1980, '1980-1-11', 'jan eleventh');
+insert into event(month, day, year, date, note) values(1, 12, 1980, '1980-1-12', 'jan twelve');
